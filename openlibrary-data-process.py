6c5f69fe1ff0128eefa5805f9d907bd8f8ba15d1
Christian Carey <christian.carey@gmail.com>
2021-07-06 18:35:22 -0700

    updates to pull all data types from the dump

--- openlibrary-data-process.py
+++ openlibrary-data-process.py
@@ -1,14 +1,34 @@
+#!/usr/bin/env python3
+
 import csv
 import sys
-input = 'data\\ol_dump_editions_2016-07-31.txt'
-output = 'data\\ol_dump_editions_2016-07-31_processed.csv'
+
+input = sys.argv[1]
+err_output = "%s_errors" % input
+
 csv.field_size_limit(sys.maxsize)
-with open(output, 'w', newline='') as csvoutputfile:
-    csvwriter = csv.writer(csvoutputfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+csv_writers = {}
+
+def open_csv(record_type):
+    return csv.writer(open("%s_%s" % (input, record_type), 'w', newline=''), delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+def write_row(record_type, row):
+    if record_type not in csv_writers:
+        csv_writers[record_type] = open_csv(record_type)
+    csv_writers[record_type].writerow([row[0], row[1], row[2], row[3], row[4]])
+
+with open(err_output, 'w') as err_ouput_file:
     with open(input, 'r') as csvinputfile:
         csvreader = csv.reader(csvinputfile, delimiter='\t')
         for row in csvreader:
             if len(row) > 4:
-                csvwriter.writerow([row[0], row[1], row[2], row[3], row[4]])
-    print('Finished reading')
+                record_type = row[0].split("/")[2]
+                write_row(record_type, row)
+            else:
+                err_output_file.write(row)
+                err_output_file.flush()
+
+        print('Finished reading')
+
 print('Finished writing')
--- openlibrary-db.sql
+++ openlibrary-db.sql
@@ -6,8 +6,8 @@ CREATE DATABASE openlibrary
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'English_United Kingdom.1252'
-       LC_CTYPE = 'English_United Kingdom.1252'
+--       LC_COLLATE = 'English_United States.1252'
+--       LC_CTYPE = 'English_United States.1252'
        CONNECTION LIMIT = -1;
 
 -- Table: public.authors
